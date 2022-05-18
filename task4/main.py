a306570af57221706246ec5f9b4d2d25bd4894e3
TatianaNaumova <105808583+TatianaNaumova@users.noreply.github.com>
2022-05-18 20:17:07 +0300

    Update main.py

--- task4/main.py
+++ task4/main.py
@@ -1,1 +1,39 @@
+import sys
+from copy import copy 
 
+
+def wall_alignment(wall):
+  wall_copy = copy(wall)
+  wall.sort()
+  mediana = 0
+  wall_length = len(wall)
+  if wall_length % 2 == 0:
+    i = wall_length // 2
+    mediana = (wall[i] + wall[i - 1]) // 2
+  else:
+    i = wall_length // 2
+    mediana = wall[i]
+
+  wall = wall_copy
+  min_steps = 0
+  for i in wall:
+    min_steps += abs(i - mediana)
+
+  steps_counter = 0
+  for i in range(wall_length):
+    while wall[i] != mediana:
+      if wall[i] < mediana:
+        wall[i] += 1
+      elif wall[i] > mediana:
+        wall[i] -= 1
+      steps_counter += 1
+  print(steps_counter)
+
+if __name__ == "__main__": 
+  filename = sys.argv[1]
+  file = open(filename, "r")
+  lines = file.readlines()
+  wall = []
+  for line in lines:
+    wall.append(int(line))
+  wall_alignment(wall)
